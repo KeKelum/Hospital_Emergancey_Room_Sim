9ba0a570b288dfb6db5834ad903ff97525b07b88
Buddhika Kelum <164610416+KeKelum@users.noreply.github.com>
2025-10-23 23:42:06 +0530

    Update HospitalER.py
    Average waiting time per priority
    Doctor utilization

--- HospitalER.py
+++ HospitalER.py
@@ -1,12 +1,13 @@
 import simpy
 import random
+import statistics
 
 # Define basic simulation setup
 RANDOM_SEED = 42  # for fixed random sequence
 ARRIVAL_RATE = 5  # average time between patient arrivals
-SERVICE_TIME = 8  # average treatment time
+SERVICE_TIME = 10  # average treatment time
 NUM_DOCTORS = 2
-SIM_TIME = 60
+SIM_TIME = 200
 
 # Priority mapping
 PRIORITY_TYPES = {
@@ -14,6 +15,11 @@ PRIORITY_TYPES = {
     "Serious": 2,
     "Minor": 3
 }
+
+wait_times = {}
+for k in PRIORITY_TYPES.keys():
+    wait_times[k] = []
+busy_time = 0 # Total doctor working time
 
 """ 
 Patient Process
@@ -25,18 +31,18 @@ def patient(env, name, doctors):
     Weights [1, 2, 4] mean:
     mostly minor cases, a few serious ones, and rare criticals.
     """
+    global busy_time
     priority_type = random.choices(list(PRIORITY_TYPES.keys()), weights=[1,2,4])[0]
     priority = PRIORITY_TYPES[priority_type]
     arrival_time = env.now
 
-    print(f"{name} ({priority_type}) arrives at {arrival_time:.2f}")
-
     with doctors.request(priority=priority) as req:
         yield req
         wait = env.now - arrival_time
-        print(f"  {name} starts at {env.now:.2f} after waiting {wait:.2f}")
+        wait_times[priority_type].append(wait)
+        start = env.now
         yield env.timeout(random.expovariate(1.0 / SERVICE_TIME))
-        print(f"  {name} ({priority_type}) leaves at {env.now:.2f}")
+        busy_time += env.now - start
 
 """ 
 Patient Arrival Generator
@@ -54,3 +60,11 @@ env = simpy.Environment()
 doctors = simpy.PriorityResource(env, capacity=NUM_DOCTORS)
 env.process(patient_arrival(env, doctors))
 env.run(until=SIM_TIME)
+
+print("\n=== Simulation Results ===")
+for k, times in wait_times.items():
+    if times:
+        print(f"{k} average wait: {statistics.mean(times):.2f} minutes")
+total_time = NUM_DOCTORS * SIM_TIME
+print(f"Doctor utilization: {busy_time / total_time * 100:.2f}%")
+
